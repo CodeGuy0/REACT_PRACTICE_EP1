0bb523669a790e13e03f6e5d5e97357809eb1552
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-02-16 16:09:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+//import the component
+import Title from "./Title.jsx";
+import Product from "./Product.jsx";
+import ProductTab from "./ProductTab.jsx";
+
+function App() {
+  return (
+    <ProductTab/>
+  );
+}
+
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+function Title () {
+    return <h1> I am the Pranay! </h1>;
+  }
+
+  // export the component 
+export default Title;
+  
